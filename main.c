01a64be0fac511e1bbab70b2d8dea94bbb29e554
Michael Bespalov <mich8bsp@gmail.com>
2021-09-13 23:18:02 +0300

    constants

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
-#include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-  printf("Hello world");
   Chunk chunk;
   initChunk(&chunk);
+
+  int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, OP_CONSTANT);
+  writeChunk(&chunk, constant);
   writeChunk(&chunk, OP_RETURN);
 
   disassembleChunk(&chunk, "test chunk");
